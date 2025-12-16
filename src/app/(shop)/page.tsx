65277d7b98eddbf6e0c1e2d7b8f7812b34dd17d4
Rodrigo <rodrigo17.ed@gmail.com>
2025-12-16 16:17:02 -0600

    ADD routes

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,7 @@
-import { titleFont } from "@/config/fonts";
-
 export default function Home() {
   return (
-    <main className=''>
-      <h1>Hola Mundo</h1>
-      <h1 className={ `${titleFont.className} font-bold` }>Hola Mundo</h1>
-    </main>
+    <div className=''>
+      <h1>Hola mundo</h1>
+    </div>
   );
 }
