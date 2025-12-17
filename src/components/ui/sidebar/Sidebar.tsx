f12a37ddef5a52dc72df6933f585393919605098
Rodrigo <rodrigo17.ed@gmail.com>
2025-12-17 15:55:54 -0600

    ADD zustand and menu animation

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import {
-  IoCloseOutline, 
+  IoCloseOutline,
   IoSearchOutline,
   IoPersonOutline,
   IoTicketOutline,
@@ -9,50 +9,73 @@ import {
   IoLogOutOutline,
   IoShirtOutline,
   IoPeopleOutline,
-} from "react-icons/io5";
+} from 'react-icons/io5';
 
-import { SidebarItem } from "./SidebarItem";
+import { SidebarItem } from './SidebarItem';
+import { useUIStore } from '@/store';
+import clsx from 'clsx';
 
 const userMenu = [
-  { href: "/", label: "Perfil", icon: IoPersonOutline },
-  { href: "/", label: "Órdenes", icon: IoTicketOutline },
-  { href: "/", label: "Ingresar", icon: IoLogInOutline },
-  { href: "/", label: "Salir", icon: IoLogOutOutline },
+  { href: '/', label: 'Perfil', icon: IoPersonOutline },
+  { href: '/', label: 'Órdenes', icon: IoTicketOutline },
+  { href: '/', label: 'Ingresar', icon: IoLogInOutline },
+  { href: '/', label: 'Salir', icon: IoLogOutOutline },
 ];
 
 const adminMenu = [
-  { href: "/", label: "Productos", icon: IoShirtOutline },
-  { href: "/", label: "Órdenes", icon: IoTicketOutline },
-  { href: "/", label: "Usuarios", icon: IoPeopleOutline },
+  { href: '/', label: 'Productos', icon: IoShirtOutline },
+  { href: '/', label: 'Órdenes', icon: IoTicketOutline },
+  { href: '/', label: 'Usuarios', icon: IoPeopleOutline },
 ];
 
 export const Sidebar = () => {
+  const isSideMenuOpen = useUIStore((state) => state.isSideMenuOpen);
+  const closeMenu = useUIStore((state) => state.closeSideMenu);
+
   return (
     <>
       {/* Overlay */}
-      <div className="fixed inset-0 z-10 bg-black/30" />
-      <div className="fixed inset-0 z-10 backdrop-blur-sm" />
+
+      {isSideMenuOpen && (
+        <>
+          <div className='fixed inset-0 z-10 bg-black/30' />
+          <div 
+            className='fixed inset-0 z-10 backdrop-blur-sm' 
+            onClick={ closeMenu }
+          />
+        </>
+      )}
 
       {/* Sidebar */}
-      <nav className="fixed right-0 top-0 z-20 h-screen w-100 bg-white p-5 shadow-2xl">
+      <nav
+        className={
+          clsx(
+            'fixed right-0 top-0 z-20 h-screen w-100 bg-white p-5 shadow-2xl duration-300',
+            {
+              'translate-x-full': !isSideMenuOpen
+            }
+          )
+        }
+      >
         <IoCloseOutline
           size={40}
-          className="absolute top-5 right-5 cursor-pointer"
+          className='absolute top-5 right-5 cursor-pointer'
+          onClick={ closeMenu }
         />
 
         {/* Search */}
-        <div className="relative mt-14">
-          <IoSearchOutline size={20} className="absolute top-2 left-2" />
+        <div className='relative mt-14'>
+          <IoSearchOutline size={20} className='absolute top-2 left-2' />
           <input
-            type="text"
-            placeholder="Buscar"
-            className="w-full rounded bg-gray-50 py-1 pl-10 pr-10 text-xl border-b-2 border-gray-200 focus:outline-none focus:border-blue-500"
+            type='text'
+            placeholder='Buscar'
+            className='w-full rounded bg-gray-50 py-1 pl-10 pr-10 text-xl border-b-2 border-gray-200 focus:outline-none focus:border-blue-500'
           />
         </div>
 
         {/* User menu */}
-        <div className="mt-10 space-y-2">
-          {userMenu.map(item => (
+        <div className='mt-10 space-y-2'>
+          {userMenu.map((item) => (
             <SidebarItem
               key={item.label}
               href={item.href}
@@ -63,11 +86,11 @@ export const Sidebar = () => {
         </div>
 
         {/* Separator */}
-        <div className="my-10 h-px w-full bg-gray-200" />
+        <div className='my-10 h-px w-full bg-gray-200' />
 
         {/* Admin menu */}
-        <div className="space-y-2">
-          {adminMenu.map(item => (
+        <div className='space-y-2'>
+          {adminMenu.map((item) => (
             <SidebarItem
               key={item.label}
               href={item.href}
