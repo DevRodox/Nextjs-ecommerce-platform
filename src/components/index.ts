e8594e6b828f470d519f65cfb1a38f73ff4327ae
Rodrigo <rodrigo17.ed@gmail.com>
2025-12-22 22:07:25 -0600

    ADD footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+
 export * from './products/product-grid/ProductGrid';
 export * from './products/product-grid/ProductGridItem';
 
@@ -7,6 +8,7 @@ export * from './product/size-selector/SizeSelector';
 export * from './product/slideshow/ProductMobileSlideshow';
 export * from './product/slideshow/ProductSlideshow';
 
+export * from './ui/footer/Footer';
 export * from './ui/not-found/PageNotFound';
 export * from './ui/sidebar/Sidebar';
 export * from './ui/sidebar/SidebarItem';
