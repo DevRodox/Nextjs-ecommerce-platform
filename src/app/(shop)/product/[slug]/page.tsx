7f4d080b85e840ad279f772e3bd6609764353832
Rodrigo <rodrigo17.ed@gmail.com>
2025-12-29 13:18:58 -0600

    ADD dynamic metadata

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,15 @@
 export const revalidate = 604800;
+import { Metadata, ResolvingMetadata } from 'next';
 
 import { notFound } from 'next/navigation';
 import { getProductBySlug } from '@/actions';
-import { ProductMobileSlideShow, ProductSlideShow, QuantitySelector, SizeSelector, StockLabel } from '@/components';
+import {
+  ProductMobileSlideShow,
+  ProductSlideShow,
+  QuantitySelector,
+  SizeSelector,
+  StockLabel,
+} from '@/components';
 import { titleFont } from '@/config/fonts';
 
 interface Props {
@@ -11,10 +18,27 @@ interface Props {
   };
 }
 
+export async function generateMetadata(
+  { params }: Props,
+): Promise<Metadata> {
+  const slug = (await params).slug;
+  const product = await getProductBySlug(slug);
+  
+  return {
+    title: product?.title,
+    description: product?.description,
+    openGraph: {
+      title: product?.title,
+      description: product?.description,
+      images: [`/products/${ product?.images[1] }`],
+    },
+  };
+}
+
 export default async function ProductPage({ params }: Props) {
   const { slug } = await params;
 
-  const product = await getProductBySlug( slug );
+  const product = await getProductBySlug(slug);
 
   if (!product) {
     notFound();
@@ -23,51 +47,44 @@ export default async function ProductPage({ params }: Props) {
   return (
     <div className='mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3'>
       {/* Slideshow */}
-      <div className='col-span-1 md:col-span-2'>        
-    
+      <div className='col-span-1 md:col-span-2'>
         {/* Mobile Slideshow */}
-        <ProductMobileSlideShow 
-          title={ product.title }
-          images={ product.images }
+        <ProductMobileSlideShow
+          title={product.title}
+          images={product.images}
           className='block md:hidden'
         />
 
         {/* Desktop Slideshow */}
-        <ProductSlideShow 
-          title={ product.title }
-          images={ product.images }
-          className='hidden md:block' 
+        <ProductSlideShow
+          title={product.title}
+          images={product.images}
+          className='hidden md:block'
         />
       </div>
 
       {/* Details */}
       <div className='col-span-1 px-5'>
-        <StockLabel slug={ product.slug }/>
-        <h1 className={`${ titleFont.className } antialised font-bold text-xl`}>
-          { product.title }
+        <StockLabel slug={product.slug} />
+        <h1 className={`${titleFont.className} antialised font-bold text-xl`}>
+          {product.title}
         </h1>
-        <p className='text-lg mb-5'>${ product.price }</p>
+        <p className='text-lg mb-5'>${product.price}</p>
 
         {/* Sizes selector */}
-        <SizeSelector 
-          selectedSize={ product.sizes[0] }
-          availableSizes={ product.sizes }
+        <SizeSelector
+          selectedSize={product.sizes[0]}
+          availableSizes={product.sizes}
         />
         {/* Quantity selector */}
-        <QuantitySelector 
-          quantity={ 2 }
-        />
-      
+        <QuantitySelector quantity={2} />
+
         {/* Button */}
-        <button className='btn-primary my-5'>
-          Agregar al carrito
-        </button>
+        <button className='btn-primary my-5'>Agregar al carrito</button>
 
         {/* Description */}
         <h3 className='font-bold text-sm'>Descripción</h3>
-        <p className='font-light'>
-          { product.description }
-        </p>
+        <p className='font-light'>{product.description}</p>
       </div>
     </div>
   );
