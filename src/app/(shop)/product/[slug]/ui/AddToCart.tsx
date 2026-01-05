c7c5ca54008ee9da2377228685ddf1d28fc2ae83
Rodrigo <rodrigo17.ed@gmail.com>
2026-01-05 12:53:18 -0600

    CHANGE QuantitySelector and AddToCart

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -5,26 +5,40 @@ import { useState } from 'react';
 import { SizeSelector, QuantitySelector } from '@/components';
 import type { Product, Size } from '@/interfaces';
 
-interface Props{
+interface Props {
   product: Product;
-};
+}
 
-export const AddToCart = ( { product }: Props ) => {
+export const AddToCart = ({ product }: Props) => {
+  const [size, setSize] = useState<Size | undefined>();
+  const [quantity, setQuantity] = useState<number>(1);
+  const [posted, setPosted] = useState(false);
 
-  const [ size, setSize ] = useState<Size|undefined>();
+  const addToCart = () => {
+    setPosted(true);
+    if (!size) return;
+    console.log({ size, quantity });
+  };
 
   return (
     <>
+      {posted && !size && (
+        <span className='mt-2 text-red-500 fade-in'>
+          Debe de seleccionar una talla*
+        </span>
+      )}
       <SizeSelector
-        selectedSize={ size }
+        selectedSize={size}
         availableSizes={product.sizes}
-        onSizeChanged={ setSize }
+        onSizeChanged={setSize}
       />
       {/* Quantity selector */}
-      <QuantitySelector quantity={ 2 } />
+      <QuantitySelector quantity={quantity} onQuantityChanged={setQuantity} />
 
       {/* Button */}
-      <button className='btn-primary my-5'>Agregar al carrito</button>
+      <button onClick={addToCart} className='btn-primary my-5'>
+        Agregar al carrito
+      </button>
     </>
   );
 };
