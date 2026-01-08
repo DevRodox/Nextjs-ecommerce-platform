74ebe4e7e49250de2f26dbba4aff68c57c2f4cd3
Rodrigo <rodrigo17.ed@gmail.com>
2026-01-08 11:30:56 -0600

    MAKE shopping cart persistent

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -4,45 +4,43 @@ import { persist } from 'zustand/middleware';
 
 interface State {
   cart: CartProduct[];
-  addProductToCart: ( product: CartProduct ) => void;
+  addProductToCart: (product: CartProduct) => void;
   //updateProductQuantity
   //removeProduct
-};
+}
 
-export const useCartStore = create<State>( 
+export const useCartStore = create(
+  persist<State>(
+    (set, get) => ({
+      cart: [],
 
-  (set, get) => ({
-    
-    cart: [],
+      addProductToCart: (product: CartProduct) => {
+        const { cart } = get();
 
-    //Methods
+        const productInCart = cart.some(
+          (item) => item.id === product.id && item.size === product.size
+        );
 
-    addProductToCart: ( product: CartProduct ) => {
-      const { cart } = get();
-      
-      //1. Revisar si el producto existe en el carrito con la talla seleccionada
-      const productInCart = cart.some(
-        (item) => item.id === product.id && item.size === product.size
-      ); 
+        if (!productInCart) {
+          set({ cart: [...cart, product] });
+          return;
+        }
 
-      if( !productInCart ){
-        set({ cart: [ ...cart, product ] });
-        return;
-      };
-
-      //2. Se que el producto existe por talla, actualizar cantidad
-      const updatedCartProducts = cart.map( (item) => {
-        if( item.id === product.id && item.size === product.size){
-          return { 
-            ...item,  
-            quantity: item.quantity + product.quantity
+        const updatedCartProducts = cart.map((item) => {
+          if (item.id === product.id && item.size === product.size) {
+            return {
+              ...item,
+              quantity: item.quantity + product.quantity,
+            };
           }
-        };
-        return item;
-      });
+          return item;
+        });
 
-      set({ cart: updatedCartProducts });
-    }
-
-  }),
+        set({ cart: updatedCartProducts });
+      },
+    }),
+    {
+      name: 'shopping-cart',
+    },
+  )
 );
